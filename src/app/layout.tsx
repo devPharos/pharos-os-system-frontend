fa76d7a0d3984a86d5be2222320f0322c86f2ae4
taugitirana <thayna@pharosit.com.br>
2023-09-11 10:48:48 -0300

    refactor: change next font to build in next font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import localFont from '@next/font/local'
+import localFont from 'next/font/local'
 
 const roboto = localFont({
   src: [
