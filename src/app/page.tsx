304c44a576baa66b024eae6d23006927093a59c4
taugitirana <thayna@pharosit.com.br>
2023-09-12 11:55:41 -0300

    feat: create card component and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
+import { Card } from '@/components/card'
 import Header from '@/layouts/header'
+import {
+  AlertCircle,
+  ArrowRightCircle,
+  CheckCircle2,
+  GaugeCircle,
+} from 'lucide-react'
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex flex-col items-center">
       <Header />
+      <main className="flex gap-6 max-w-7xl w-full px-6 py-14">
+        <Card
+          color="gray-100"
+          icon={GaugeCircle}
+          label="Tickets em aberto"
+          quantity={32}
+        />
+
+        <Card
+          color="red-500"
+          icon={AlertCircle}
+          label="Tickets em atraso"
+          quantity={2}
+        />
+
+        <Card
+          color="yellow-500"
+          icon={ArrowRightCircle}
+          label="Tickets em andamento"
+          quantity={12}
+        />
+
+        <Card
+          color="green-500"
+          icon={CheckCircle2}
+          label="Tickets concluídos esse mês"
+          quantity={52}
+        />
+      </main>
     </div>
   )
 }
---
+++ src/components/card/index.tsx
@@ -0,0 +1,21 @@
+import { ElementType } from 'react'
+
+interface CardProps {
+  icon: ElementType
+  label: string
+  color: string
+  quantity: number
+}
+
+export function Card({ icon: Icon, label, color, quantity }: CardProps) {
+  return (
+    <div className="w-full flex flex-col gap-4 rounded-lg bg-gray-700 p-6">
+      <header className="flex items-center justify-between">
+        <span className="text-sm font-medium text-gray-200">{label}</span>
+        <Icon className="w-4 h-4 text-gray-200" />
+      </header>
+
+      <span className={`text-2xl font-bold text-${color}`}>{quantity}</span>
+    </div>
+  )
+}
